fca1e68b9b73e5de730e2e2521a9e9018e861e2f
lionelyau23 <lionel1997613@gmail.com>
2022-01-23 18:26:48 +0800

    completed ex 3.3

--- index.js
+++ index.js
@@ -1,1 +1,55 @@
-console.log("hello world")
+const express = require('express')
+const app = express()
+
+let data = [
+    { 
+      "id": 1,
+      "name": "Arto Hellas", 
+      "number": "040-123456"
+    },
+    { 
+      "id": 2,
+      "name": "Ada Lovelace", 
+      "number": "39-44-5323523"
+    },
+    { 
+      "id": 3,
+      "name": "Dan Abramov", 
+      "number": "12-43-234345"
+    },
+    { 
+      "id": 4,
+      "name": "Mary Poppendieck", 
+      "number": "39-23-6423122"
+    }
+]
+
+app.use(express.json())
+
+app.get('/api/persons', (request, response) => {
+    response.json(data)
+})
+
+app.get('/info', (request, response) => {
+    response.send(`
+    <p>Phonebook has info for ${data.length} people</p>
+    <p>${new Date()}</p>
+    `)
+})
+
+app.get('/api/persons/:id', (request, response) => {
+    const id = Number(request.params.id)
+    const person = data.find(person => person.id === id)
+
+    if (person) {
+        response.json(person)
+    } else {
+        response.statusMessage = 'no such id exists'
+        response.status(404).end()
+    }
+})
+
+const PORT = 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`)
+})
