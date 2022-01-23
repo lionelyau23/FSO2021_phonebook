44e654ff43856db796dc7d5da7f8069f6da54358
lionelyau23 <lionel1997613@gmail.com>
2022-01-23 18:35:54 +0800

    completed ex 3.5

--- index.js
+++ index.js
@@ -56,6 +56,19 @@ app.delete('/api/persons/:id', (request, response) => {
     response.status(204).end()
 })
 
+app.post('/api/persons', (request, response) => {
+    const body = request.body
+
+    const person = {
+        name: body.name,
+        number: body.number,
+        id: Math.round(Math.random() * 1000)
+    }
+
+    data = data.concat(person)
+    response.json(person)
+})
+
 const PORT = 3001
 app.listen(PORT, () => {
     console.log(`Server running on port ${PORT}`)
